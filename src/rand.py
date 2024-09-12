1becffe65227abcd16a1b91e53aca7b11f251c60
valorant-dhruv <78591597+valorant-dhruv@users.noreply.github.com>
2024-09-12 06:35:07 +0000

    refactored code working now

--- src/rand.py
+++ src/rand.py
@@ -1,10 +1,12 @@
+"""Module providing a function to generate random array values based on a given array of n size."""
 import subprocess
 
 
 def random_array(arr):
+    """Function generating a random array."""
     shuffled_num = None
-    for i in range(len(arr)):
+    for element in enumerate(arr):
         shuffled_num = subprocess.run(
-            ["shuf", "-i1-20", "-n1"], capture_output=True)
-        arr[i] = int(shuffled_num.stdout)
+            ["shuf", "-i1-20", "-n1"], capture_output=True,check=True)
+        arr[element[0]] = int(shuffled_num.stdout)
     return arr
