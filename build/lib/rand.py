d9a91dd6cc596817712f7ce9fbf2bff9da5b863d
valorant-dhruv <78591597+valorant-dhruv@users.noreply.github.com>
2024-09-12 17:51:35 +0000

    pyproject.toml config

---
+++ build/lib/rand.py
@@ -0,0 +1,12 @@
+"""Module providing a function to generate random array values based on a given array of n size."""
+import subprocess
+
+
+def random_array(arr):
+    """Function generating a random array."""
+    shuffled_num = None
+    for element in enumerate(arr):
+        shuffled_num = subprocess.run(
+            ["shuf", "-i1-20", "-n1"], capture_output=True, check=True)
+        arr[element[0]] = int(shuffled_num.stdout)
+    return arr
