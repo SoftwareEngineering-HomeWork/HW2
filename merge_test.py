ffe2db488f950812cade4ed9b3b1a29564688285
valorant-dhruv <78591597+valorant-dhruv@users.noreply.github.com>
2024-09-12 18:00:05 +0000

    testing hooks

---
+++ merge_test.py
@@ -0,0 +1,15 @@
+
+def testsort():
+    tosort = [1,2,3,4,5,6]
+    expectedsort=[1,2,3,4,5,6]
+    assert tosort==expectedsort
+
+def testsort2():
+    tosort = [1,2,3,4,5,6]
+    expectedsort=[1,2,3,4,5,6]
+    assert tosort==expectedsort
+
+def testsort3():
+    tosort = [1,2,3,4,5,6]
+    expectedsort=[1,2,3,4,5,6]
+    assert tosort==expectedsort
